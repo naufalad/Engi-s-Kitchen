bf7b37dfc488732a67d2ac2bb9940a115f648821
naufalad <naufalkps@gmail.com>
2018-11-14 01:00:21 +0700

    Add files via upload

--- tampilan.c
+++ tampilan.c
@@ -1,26 +1,40 @@
 #include <stdio.h>
 #include "boolean.h"
+#include <string.h>
 
 int main(){
-    printf("\n---------------------------------------------------------------\n");
-    printf("|<NAME>          |Money: 500 |Life: 2    |Time: 5             |\n");
-    printf("|-------------------------------------------------------------|\n");
-    printf("|Waiting Cust    |   | C |   |   |   |   |   |   |Food Stack  |\n");
-    printf("|                |-------------------------------|            |\n");
-    printf("|2               | C | 1 | C |   |   | C | 2 | C |            |\n");
-    printf("|                |-------------------------------|            |\n");
-    printf("|                |   | C |   |   | P |   |   |   |            |\n");
-    printf("|                |-------------------------------|            |\n");
-    printf("|                |   |   |   |   |   |   |   |   |            |\n");
-    printf("--------------------------------------------------------------|\n");
-    printf("|Order           |   |   |   |   |   |   |   |   |Hand        |\n");
-    printf("|                |-------------------------------|            |\n");
-    printf("|                |   | X |   |   |   |   |   |   |            |\n");
-    printf("|                |-------------------------------|            |\n");
-    printf("|                | X | 3 | X |   |   | C | 4 | C |            |\n");
-    printf("|                |-------------------------------|            |\n");
-    printf("|                |   | X |   |   |   |   |   |   |            |\n");
-    printf("|-------------------------------------------------------------|\n");
-    printf("|Command: GU                                                  |\n");
-    printf("---------------------------------------------------------------\n");
+    char kosong = ' ';
+    char player = 'P';
+    char kursikosong = 'X';
+    char meja1 = '1';
+    char meja2 = '2';
+    char meja3 = '3';
+    char meja4 = '4';
+    char konsumen = 'C';
+    char nama[] = "ASDFGH";
+    char command[2];
+    int uang = 500;
+    int life = 100;
+    int time = 5;
+    printf("\n┌────────────────┬───────────────┬───────────────┬────────────┐\n");
+    printf("│%s\t\t │ Money: %d\t │ Life: %d\t │Time: %d     │\n", nama, uang, life, time);
+    printf("├────────────────┼───┬───┬───┬───┼───┬───┬───┬───┼────────────┤\n");
+    printf("│Waiting Cust    │ %c │ %c │ %c │ %c │ %c │ %c │ %c │ %c │Food Stack  │\n", kosong, konsumen, kosong, kosong, kosong, kosong, kosong, kosong);
+    printf("│                ├───┼───┼───┼───┼───┼───┼───┼───│            │\n");
+    printf("│2               │ %c │ %c │ %c │ %c │ %c │ %c │ %c │ %c │            │\n", konsumen, meja1, konsumen, player, kosong, konsumen, meja2, konsumen);
+    printf("│                ├───┼───┼───┼───┼───┼───┼───┼───│            │\n");
+    printf("│                │ %c │ %c │ %c │ %c │ %c │ %c │ %c │ %c │            │\n", kosong, konsumen, kosong, kosong, kosong, kosong, kosong, kosong);
+    printf("│                ├───┼───┼───┼───┼───┼───┼───┼───│            │\n");
+    printf("│                │ %c │ %c │ %c │ %c │ %c │ %c │ %c │ %c │            │\n", kosong, kosong, kosong, kosong, kosong, kosong, kosong, kosong);
+    printf("├────────────────┼───┼───┼───┼───┼───┼───┼───┼───┼────────────┤\n");
+    printf("│Order           │ %c │ %c │ %c │ %c │ %c │ %c │ %c │ %c │Hand        │\n", kosong, kosong, kosong, kosong, kosong, kosong, kosong, kosong);
+    printf("│                ├───┼───┼───┼───┼───┼───┼───┼───│            │\n");
+    printf("│                │ %c │ %c │ %c │ %c │ %c │ %c │ %c │ %c │            │\n", kosong, kursikosong, kosong, kosong, kosong, kosong, kosong, kosong);
+    printf("│                ├───┼───┼───┼───┼───┼───┼───┼───│            │\n");
+    printf("│                │ %c │ %c │ %c │ %c │ %c │ %c │ %c │ %c │            │\n", kursikosong, meja3, kursikosong, kosong, kosong, kursikosong, meja4, kursikosong);
+    printf("│                ├───┼───┼───┼───┼───┼───┼───┼───│            │\n");
+    printf("│                │ %c │ %c │ %c │ %c │ %c │ %c │ %c │ %c │            │\n", kosong, kursikosong, kosong, kosong, kosong, kosong, kosong, kosong);
+    printf("└────────────────┴───┴───┴───┴───┴───┴───┴───┴───┴────────────┘\n");
+    printf(" Command : ");
+    scanf("%s", command);
 }
