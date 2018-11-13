d847287143c76d6ffb70f1ed428832ae527864e7
naufalad <naufalkps@gmail.com>
2018-11-14 00:59:55 +0700

    main program

--- main.c
+++ main.c
@@ -2,12 +2,14 @@
 
 #include <stdio.h>
 #include "boolean.h"
-//include semua
+#include "ADT.h"
+#include "command.h"
 
 int main()
 {
 	int pilihan;
 	boolean StartGame = false;
+	boolean exit = false;
 	
 	printf("===============SELAMAT DATANG DI ENGI'S KITCHEN EXPANSION!===============\n\n");
 
@@ -24,42 +26,52 @@ int main()
 		switch (pilihan){
 			case 1 :
 			printf("Masukkan nama anda : ");
-			scanf("%d", Pemain.nama);
+			//input kata pemain.nama
 			//apakah mau di validasi inputannya?
 			printf("Untuk mulai permainan, silahkan pilih Start Game pada Main Menu.\n")
 			break;
-			//scanf nama
 			
 			case 2 :
-			if(Pemain.nama != Nil) StartGame = true;
+			if(Pemain.nama != NULL) StartGame = true;
 			else printf("Nama belum di input! Silahkan kembali ke main menu dan pilih New Game atau Load Game.\n")
 			break;
 			//ngecek udh ada namanya atau belom, kalo belom suruh input lg
 
 			case 3 : 
-			//Status pemain(nama, life, money, time, posisi), queue, hand, nampan dll
+			LoadFile();
 			printf("Untuk mulai permainan, silahkan pilih Start Game pada Main Menu.\n")
 			//read file eksternal
 			case 4 :
 			ExitGame(false);
+			exit = true;
 			break;
 		
 				
-	}while(StartGame == false);
-	//StartGame udh true
-	printf("\nSelamat datang, Chef.\n\n");
-	printf("Dikisahkan pada suatu hari, anda sebagai seorang Chef membuka suatu restoran bernama Engi's Kitchen. Hanya dalam satu tahun, restoran tersebut berkembang pesat dan sudah bisa dibilang sukses dan berjaya.\n");
-	printf("Sekarang anda berencana mengembangkan bisnis ini lebih lanjut namun sebelum itu, anda mencoba mensimulasikannya terlebih dahulu dengan program ini.\n");
-	printf("Anggaplah simulasi ini seperti anda menjalankan restoran yang sebenarnya, Selamat bermain!\n");
-	
-	//do{ TampilanProgramUtama() }while command!=Exit
-	/*if command == exit
-		mau disave?
-		if y ExitGame(true)
-		else ExitGame(false)
-	*/
+	}while((StartGame == false)&&(exit==false));
+	if(StartGame){
+		printf("\nSelamat datang, Chef");
+		//output nama
+		printf(".\n\n");
+		printf("Dikisahkan pada suatu hari, anda sebagai seorang Chef membuka suatu restoran bernama Engi's Kitchen. Hanya dalam satu tahun, restoran tersebut berkembang pesat dan sudah bisa dibilang sukses dan berjaya.\n");
+		printf("Sekarang anda berencana mengembangkan bisnis ini lebih lanjut namun sebelum itu, anda mencoba mensimulasikannya terlebih dahulu dengan program ini.\n");
+		printf("Anggaplah simulasi ini seperti anda menjalankan restoran yang sebenarnya, Selamat bermain!\n");
+		
+		/*do{ 
+			TampilanProgramUtama() 
+			switch command{
+				case "GU":
+				GU();
+				break;
+			}
+			//buat ngelakuin sesuai inputan, pake switch case
+		}while (command!=Exit);*/
+		printf("Jam Sekarang : ");
+		//output RealTime
+		printf("Mau disave?[1 untuk Ya]");
+		scanf("%d", &pilihan);
+		if (pilihan) ExitGame(true);
+		else ExitGame(false);
 
 
 	return 0;
 }
-
