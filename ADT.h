70f135e8f01060d160eedea4f4bcf02b52363e55
Ahmad Rizal Alifio <44598264+ARAlifio@users.noreply.github.com>
2018-11-23 17:01:35 +0700

    Add files via upload

--- ADT.h
+++ ADT.h
@@ -1,25 +1,46 @@
-/* File : adt.h */
+/* File : ADT.h */
 /* kumpulan struktur data*/
 
-//disini kita #include semua ADT praktikum
 
 #ifndef adt_H
-	#define adt_H
+#define adt_H
+
+	#include "boolean.h"
+	#include "point.h"
+	#include "jam.h"
+	#include "mesinkar.h"
+	#include "mesinkata.h"
+	#include "arraykata.h"
+	#include "matriks.h"
+	#include "stackt.c"
+	#include "prioqueue.h"
 
 	typedef struct{
 		Kata nama;
-		long money;
-		long life;
-		long time;
+		int money;
+		int life;
+		int time;
 		POINT posisi;
-	}Player;
+	} Player;
 
+	typedef struct{
+		POINT posisi;
+		int kursi;
+
+	} MejaMakan;
+
+	typedef struct{
+		POINT posisi;
+		Kata bahan;
+	} MejaDapur;
+
+	
 	static Player Pemain;
-	static JAM RealTime;
+	static JAM RealTime = CurrentTime();
 	//bakal disesuain sama real time, buat dipake di savegame
 	//bakalan NextNDetik mulu tiap program berjalan(kecuali ga harus real time, kalo ga harus ntar bisa diconvert dr lama main)
 
-	static TabString Pesanan;
+	static TabKata Pesanan;
 	//ubah array.h buat orderan, ElType = Kata(atau mesin kata?)
 
 	static MATRIKS Ruangan1;
@@ -27,7 +48,7 @@
 	static MATRIKS Ruangan3;
 	static MATRIKS Dapur;
 	//ubah matriks.h buat representasi ruangan, ElType tetep int, tapi kalo kosong=0, Meja=1-4, Player(P) = 5, Kursi kosong(X) = 6, kursi ada(C) = 7, Meja naroh bahan(M) = 8, Tray(T) = 9
-
+	static Kata command;
 	//Mesin Karakter + Mesin Kata buat baca informasi player dr file eksternal, informasi peta(skrg lg dimana dll), baca command dr pengguna, sama baca savefile
 
 	static Queue Antrian;
@@ -41,7 +62,7 @@
 
 	//List buat Implementasi graf(?)
 
-	//Tree buat resep <- belom diajarin
+	//Tree buat resep
 
 	//Graph buat "world"nya <- carry us bim, kata kakaknya tanya ke anggota yg udh cp aja
 
@@ -55,7 +76,7 @@
 	printf("Ahmad Rizqee Nurhani/13517058\n");
 	printf("Naufal Aditya Dirgandhavi/13517064\n");
 	printf("Ahmad Rizal Alifio/13517076\n\n");*/
-	
+
 	void TampilanProgramUtama();
 	//ntar bakal nampilin program utamanya
 	void SaveFile();
