bb23ad7d5c20b9c6024a2d5d39665c826591e63d
naufalad <naufalkps@gmail.com>
2018-11-23 23:17:52 +0700

    Menambah arraypesanan

--- ADT.h
+++ ADT.h
@@ -10,9 +10,9 @@
 	#include "jam.h"
 	#include "mesinkar.h"
 	#include "mesinkata.h"
-	#include "arraykata.h"
+	#include "arraypesanan.h"
 	#include "matriks.h"
-	#include "stackt.c"
+	#include "stackt.h"
 	#include "prioqueue.h"
 	#include "bintree.h"
 
@@ -22,62 +22,83 @@
 		int life;
 		int time;
 		POINT posisi;
-		int ruangan; /*ada di ruangan berapa */
+		int ruangan;
 	} Player;
-
+	Player Pemain;
+	JAM RealTime;
 	typedef struct{
 		POINT posisi; //mejanya ada di titik berapa
 		int kursi; //ada berapa customer yang bisa ditempatkan disitu
-		int ID; //meja no berapa di ruangan
-		int Cust; //ada berapa customer yang duduk disitu
+		int NCustomer;
 	} MejaMakan;
 
 	typedef struct{
 		POINT posisi;
 		Kata bahan;
 	} MejaDapur;
-
-	Player Pemain;
-	JAM RealTime
 	//bakal disesuain sama real time, buat dipake di savegame
 	//bakalan NextNDetik mulu tiap program berjalan(kecuali ga harus real time, kalo ga harus ntar bisa diconvert dr lama main)
 
-	static TabKata Pesanan;
+	TabPesanan Pesanan;
 	//ubah array.h buat orderan, ElType = Kata(atau mesin kata?)
+	typedef struct{
+		MATRIKS M;
+		POINT P1;
+		POINT P2;
+		MejaMakan M1;
+		MejaMakan M2;
+		MejaMakan M3;
+		MejaMakan M4;
+	}Ruang;
+	Ruang Ruangan1;
+	Ruang Ruangan2;
+	Ruang Ruangan3;
+	typedef struct{
+		POINT posisi;
+		Stack bahan;
+	}Tray;
+	typedef struct{
+		MATRIKS M;
+		POINT P1;
+		POINT P2;
+		Tray T;
+		MejaDapur M1;
+		MejaDapur M2;
+		MejaDapur M3;
+		MejaDapur M4;
+		MejaDapur M5;
+		MejaDapur M6;
+		MejaDapur M7;
+		MejaDapur M8;
+		MejaDapur M9;
+		MejaDapur M10;
+		MejaDapur M11;
+		MejaDapur M12;
+		MejaDapur M13;
+		MejaDapur M14;
+		MejaDapur M15;
+	}RuangDapur;
 
-	static MATRIKS Ruangan1;
-	static MATRIKS Ruangan2;
-	static MATRIKS Ruangan3;
-	static MATRIKS Dapur;
+	RuangDapur Dapur;
 	//ubah matriks.h buat representasi ruangan, ElType tetep int, tapi kalo kosong=0, Meja=1-4, Player(P) = 5, Kursi kosong(X) = 6, kursi ada(C) = 7, Meja naroh bahan(M) = 8, Tray(T) = 9
-	static Kata command;
+	Kata command;
 	//Mesin Karakter + Mesin Kata buat baca informasi player dr file eksternal, informasi peta(skrg lg dimana dll), baca command dr pengguna, sama baca savefile
 
-	static Queue Antrian;
-	//Prioqueue buat antrean, isinya bakalan 4 sama 2, plus ditambahin variabel waktu nunggunya(30 satuan waktu)
+	Queue Antrian;
+	//Prioqueue buat antrian, isinya bakalan 4 sama 2, plus ditambahin variabel waktu nunggunya(30 satuan waktu)
 
-	static Stack Makanan;
-	static Stack Bahan;
+	Stack Makanan;
+	Stack Tangan;
 	//Stack buat tumpukan makanan di tangan dan di nampan, infotype = Kata(atau mesin kata?)
 	//urg bingung, apakah tumpukan di tangan itu maksudnya makanan yg lg dibawa, terus di nampan itu yg lg di tempat bikin makan dan itu dibuat stack jg?
 
 
 	//List buat Implementasi graf(?)
 
-	//Tree buat resep
+	BinTree Resep;
 
-	//Graph buat "world"nya <- carry us bim, kata kakaknya tanya ke anggota yg udh cp aja
 
 	void ExitMessage(boolean SaveGame);
-	//ntar bakal dipake di command exit sama exitnya main menu, dia bakal nampilin message kalo keluar game dan bakalan ngesave kalo savegamenya true
-	//tampilin credit pembuat game
-	printf("Kelas K-1\n");
-	printf("Kelompok Warteg Bahari\n");
-	printf("Bimo Adityarahman Wiraputra/13517004\n");
-	printf("Ricky Yuliawan Pohan/13517025\n");
-	printf("Ahmad Rizqee Nurhani/13517058\n");
-	printf("Naufal Aditya Dirgandhavi/13517064\n");
-	printf("Ahmad Rizal Alifio/13517076\n\n"); 
 
 	void TampilanProgramUtama();
 	//ntar bakal nampilin program utamanya
@@ -87,5 +108,10 @@
 	//bakal dipake sama main menu yg Load game
 	char MatriksToChar(int y);
 	//ubah isi matriks jadi char
+	//BinTree assignResep();
+	void init_ruangan1(Ruang *Ruangan1);
+	void init_ruangan2(Ruang *Ruangan1);
+	void init_ruangan3(Ruang *Ruangan1);
+	void init_dapur(RuangDapur *Dapur);
 
 #endif
